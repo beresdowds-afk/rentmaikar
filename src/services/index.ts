c09faa337395551689610212e067fe3d3a8f4fb0
Olusola Adebayo <adebayoolusola39@gmail.com>
2026-07-21 23:48:30 +0100

    Add exports for ResidentOrchestrator and bridges

---
+++ src/services/index.ts
@@ -0,0 +1,16 @@
+export {
+default as ResidentOrchestrator
+}
+from "./residentOrchestrator";
+
+
+export {
+receiveTraccarEvent
+}
+from "./traccarBridge";
+
+
+export {
+receiveMQTTMessage
+}
+from "./mqttBridge";
