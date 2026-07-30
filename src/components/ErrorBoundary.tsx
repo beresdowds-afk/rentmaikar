a2855eda949e6862d6e1212380c6375879f57c68
Olusola Adebayo <adebayoolusola39@gmail.com>
2026-07-30 13:01:31 +0100

    Change retry behavior to reload the page
    Reload the page on retry instead of resetting state.

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -32,9 +32,10 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   handleRetry = () => {
-    this.setState({ hasError: false, error: null, errorInfo: null });
-  };
 
+window.location.reload();
+
+};
   handleGoHome = () => {
     window.location.href = "/";
   };
